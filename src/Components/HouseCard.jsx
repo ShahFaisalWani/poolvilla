74f020c2169f8f3f38cc494f9b16616d4ee9f8d3
ShahFaisalWani <shahfaisalwani14@gmail.com>
2024-07-04 16:35:30 +0700

    house card clean up

--- src/Components/HouseCard.jsx
+++ src/Components/HouseCard.jsx
@@ -1,45 +1,42 @@
 import React from "react";
 
-const HouseCard = ({ house }) => {
-  const title = house.code + " " + house.district;
-  return (
-    <a className="house-card" href={"/house/" + house.code} target="_blank">
-      <p className="tag">{house.location.name}</p>
-      <div className="house-image">
-        <img
-          src={
-            "https://sgp1.digitaloceanspaces.com/villapaza-spaces" +
-            house.picture_house[0]
-          }
-          alt={`House ${house.code}`}
-        />
-      </div>
-      <div className="house-details">
-        <h3 className="title">
-          {title.length > 23 ? title.substring(0, 23) + ".." : title}
-        </h3>
-        <p className="price">
+const HouseCard = ({ house }) => (
+  <a className="house-card" href={"/house/" + house.code} target="_blank">
+    <p className="tag">{house.location.name}</p>
+    <div className="house-image">
+      <img
+        src={
+          "https://sgp1.digitaloceanspaces.com/villapaza-spaces" +
+          house.picture_house[0]
+        }
+        alt={`House ${house.code}`}
+      />
+    </div>
+    <div className="house-details">
+      <div className="top">
+        <div className="code">{house.code}</div>
+        {house.district && <div className="district">{house.district}</div>}
+        <div className="price">
           ราคา ฿ {house.lowestPrice.price.toLocaleString()}
-        </p>
-        <div className="line" />
-        <div className="features">
-          <p>
-            <i className="ri-team-line"></i> สูงสุด {house.accommodate_number}
-          </p>
-          <p>
-            {" "}
-            <i className="ri-hotel-bed-line"></i>
-            {house.number_of_bedrooms}
-          </p>
-          <p>
-            {" "}
-            <i className="ri-door-closed-line"></i>
-            {house.number_of_bathrooms}
-          </p>
         </div>
       </div>
-    </a>
-  );
-};
+      <div className="features">
+        <div>
+          <i className="ri-team-line"></i> สูงสุด {house.accommodate_number}
+        </div>
+        <div>
+          {" "}
+          <i className="ri-hotel-bed-line"></i>
+          {house.number_of_bedrooms}
+        </div>
+        <div>
+          {" "}
+          <i className="ri-door-closed-line"></i>
+          {house.number_of_bathrooms}
+        </div>
+      </div>
+    </div>
+  </a>
+);
 
 export default HouseCard;
