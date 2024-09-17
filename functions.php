44f330ffa711dbece266afb4f283253f3a06abb1
ShahFaisalWani <shahfaisalwani14@gmail.com>
2024-09-17 22:22:20 +0700

    mobile padding, rank math seo

--- functions.php
+++ functions.php
@@ -28,3 +28,53 @@ add_action( 'template_include', function( $template ) {
 } );
 
 wp_enqueue_script( 'react-search', get_stylesheet_directory_uri() . "/build/main-brikk.js", [], true);
+
+function custom_title( $title ) {
+	$url = $_SERVER['REQUEST_URI'];
+
+	// Check if the URL matches the pattern /house/*
+	if (strpos($url, '/house/') !== false) {
+			$parts = explode('/', $url);
+			$city_code = '';
+
+			foreach ($parts as $part) {
+					if (strpos($part, 'CITY-') !== false) {
+							$city_code = $part;
+							break;
+					}
+			}
+
+			$title1 = $title;
+			if (!empty($city_code)) { 
+					$api_url = 'https://api.poolvillacity.co.th/next-villapaza/api/customer/house/info/' . strtoupper($city_code);
+					$response = wp_remote_get($api_url);
+					if (!is_wp_error($response)) {
+							$data = json_decode(wp_remote_retrieve_body($response), true);
+							$data = $data["result"];
+							$house =  $data["house"];
+							$name =  $house["name"];
+							
+							$title1 = "{$house['name']} {$house['location']['name']} {$house['code']} | {$house['number_of_bedrooms']} ห้องนอน Casa Poolvilla";    
+					}
+			}
+
+			return $title1;
+	}
+
+	return $title;
+}
+
+function custom_desc( $description ) {
+	$url = $_SERVER['REQUEST_URI'];
+
+	if (strpos($url, '/house/') !== false) {
+			return false; 
+	}
+
+	return $description;
+}
+
+add_filter('rank_math/frontend/title', 'custom_title');
+add_filter('rank_math/frontend/description', 'custom_desc');
+add_filter('rank_math/opengraph/facebook/og_description', 'custom_desc');
+add_filter('rank_math/opengraph/twitter/twitter_description', 'custom_desc');
